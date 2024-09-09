64277a4a6a7defe9658a009fef3fa86f33a812c4
KrisPersson <112572136+KrisPersson@users.noreply.github.com>
2024-09-09 22:55:00 +0200

    quick start

--- main.js
+++ main.js
@@ -5,7 +5,34 @@ document.querySelector('#app').innerHTML = `
  <div id="map"></div>
 `
 
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([57.652954, 11.911073], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+var marker = L.marker([51.5, -0.09]).addTo(map);
+var circle = L.circle([57.642954, 11.911073], {
+  color: 'red',
+  fillColor: '#f03',
+  fillOpacity: 0.3,
+  radius: 500
+}).addTo(map);
+var popup = L.popup();
+
+var polygon = L.polygon([
+  [57.664029, 11.873818],
+  [57.654857, 11.871715],
+  [57.660346, 11.888495]
+]).addTo(map);
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
 console.log(map)
 
 setupCounter(document.querySelector('#counter'))
